7b466d664a5dccad14047d1345f831cd81eaa63e
Vinicius Andrade <Viniciusjandrade08@gmail.com>
2023-11-01 10:30:04 -0300

    adicionando mega-senha

--- Treinamento/src/script.js
+++ Treinamento/src/script.js
@@ -9,7 +9,7 @@ class Quadrado {
         console.log(`Área do quadrado: ${this.valor * this.valor}`);
     }
     Perimetro() {
-        console.log(`Área do quadrado: ${this.valor * this.lado}`);
+        console.log(`Perimetro: ${this.valor * this.lado}`);
     }
 }
 function Rodar() {
@@ -26,6 +26,66 @@ function Rodar() {
 const usuario = {
     fistName: 'vinicius',
     LastName: 'andrade',
-    age: 20
+    age: 20,
+    loteria: [],
+    sortiado: [10, 20, 30, 40, 50, 60],
+    tentativas: 0,
+    pontos: 0
 };
-console.log(usuario.fistName);
+const btntentar = document.querySelector('.btn-tentar');
+const btnloteria = document.querySelector('.btn-loteria');
+function Loteria() {
+    const numerosLoteria = document.querySelectorAll('.inputLoteria');
+    numerosLoteria.forEach((item) => {
+        const numeroselecionado = +item.value;
+        usuario.loteria.push(numeroselecionado);
+    });
+    for (let i = 0; i < 5; i++) {
+        if (usuario.sortiado[i] == usuario.loteria[i]) {
+            console.log(`Parabens acertasse: ${usuario.sortiado[i]}`);
+            usuario.pontos = usuario.pontos + 1;
+            if (usuario.pontos == 5) {
+                alert(`ALELEUIAAAAA \n Foram necessários: ${usuario.tentativas} tentativas`);
+            }
+        }
+    }
+    console.log(`Numeros que voce sorteou: ${usuario.loteria}`);
+    usuario.tentativas += 1;
+    if (btnloteria) {
+        btnloteria.disabled = true;
+    }
+    if (btntentar) {
+        btntentar.disabled = false;
+    }
+}
+function tentar() {
+    LimparInput();
+    limparArray();
+    if (btnloteria) {
+        btnloteria.disabled = false;
+    }
+    if (btntentar) {
+        btntentar.disabled = true;
+    }
+}
+function LimparInput() {
+    const numerosLoteria = document.querySelectorAll('.inputLoteria');
+    numerosLoteria.forEach((item) => {
+        item.value = "";
+    });
+}
+function limparArray() {
+    usuario.loteria = [];
+    usuario.pontos = 0;
+}
+const inputs = document.querySelectorAll('.inputLoteria');
+inputs.forEach((input, index) => {
+    input.addEventListener('keydown', (e) => {
+        if (e.key === 'Enter' || e.keyCode === 13) {
+            e.preventDefault(); // Impede o envio do formulário padrão
+            if (index < inputs.length - 1) {
+                inputs[index + 1].focus(); // Mova o foco para o próximo input
+            }
+        }
+    });
+});
--- Treinamento/src/script.ts
+++ Treinamento/src/script.ts
@@ -14,7 +14,7 @@ class Quadrado{
   }
 
   Perimetro(){
-    console.log(`Área do quadrado: ${this.valor * this.lado}`)
+    console.log(`Perimetro: ${this.valor * this.lado}`)
   }
 }
 
@@ -35,18 +35,100 @@ function Rodar(){
 
 
 
-
 type User = {
   fistName:string,
   LastName:string,
-  age:number
+  age:number,
+  loteria:number[],
+  sortiado:number[],
+  tentativas: number,
+  pontos:number
 }
 
 const usuario:User = {
   fistName:'vinicius',
   LastName:'andrade',
-  age:20
+  age:20,
+  loteria:[],
+  sortiado:[10,20,30,40,50,60],
+  tentativas:0,
+  pontos:0
+
+
+}
+const btntentar: HTMLButtonElement | null = document.querySelector('.btn-tentar');
+const btnloteria: HTMLButtonElement | null = document.querySelector('.btn-loteria');
+
+function Loteria(){
+  const numerosLoteria: NodeListOf<HTMLInputElement> = document.querySelectorAll('.inputLoteria');
+
+  numerosLoteria.forEach((item) => {
+    const numeroselecionado:number = +item.value
+    usuario.loteria.push(numeroselecionado)
+
+  })
+
+  for(let i=0;i<5;i++){
+    if(usuario.sortiado[i] == usuario.loteria[i]){
+      console.log(`Parabens acertasse: ${usuario.sortiado[i]}`)
+      usuario.pontos = usuario.pontos + 1
+
+      if(usuario.pontos == 5){
+        alert(`ALELEUIAAAAA \n Foram necessários: ${usuario.tentativas} tentativas`)
+        
+      }
+    }
+  }
+
+  console.log(`Numeros que voce sorteou: ${usuario.loteria}`)
+  usuario.tentativas+= 1
+  
+  if (btnloteria) {
+    btnloteria.disabled = true;
+  }
+
+  if (btntentar) {
+    btntentar.disabled = false;
+  }
+  
 
 }
 
-console.log(usuario.fistName)
+function tentar(){
+  LimparInput()
+  limparArray()
+
+  if (btnloteria) {
+    btnloteria.disabled = false;
+  }
+  if (btntentar) {
+    btntentar.disabled = true;
+  }
+}
+
+function LimparInput(){
+
+  const numerosLoteria: NodeListOf<HTMLInputElement> = document.querySelectorAll('.inputLoteria');
+  numerosLoteria.forEach((item) => {
+    item.value = ""
+  })
+}
+
+function limparArray(){
+  usuario.loteria = [];
+  usuario.pontos = 0
+}
+
+const inputs = document.querySelectorAll('.inputLoteria') as NodeListOf<HTMLInputElement>;
+
+inputs.forEach((input, index) => {
+  input.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter' || e.keyCode === 13) {
+      e.preventDefault(); // Impede o envio do formulário padrão
+
+      if (index < inputs.length - 1) {
+        (inputs[index + 1] as HTMLInputElement).focus(); // Mova o foco para o próximo input
+      }
+    }
+  });
+});
